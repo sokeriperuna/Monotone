5b611be24bf50612d609048a7b111b2c8c214737
Esko Evtyukov <eskoonwheels@gmail.com>
2018-09-19 19:40:38 +0300

    Started prototyping 2D platformer mechanics

---
+++ Monotone/Assets/Scenes/Scripts/PlayerController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    private PlayerEntity playerEntity;
+
+    private float hInput;
+    private float jumpStopTime;
+
+    private bool jumping = false;
+    private bool verticalMomentumStopped = false;
+
+    public float jumpStopInterval;
+
+    void Awake()
+    {
+        playerEntity = GetComponent<PlayerEntity>();
+    }
+
+	void FixedUpdate ()
+    {
+        hInput = GetHorizontalInput();
+        if (hInput != 0)
+            playerEntity.Move(hInput);
+
+        if (!jumping && Input.GetKeyDown(KeyCode.Space))
+        {
+            jumping = true;
+            jumpStopTime = jumpStopInterval + Time.time;
+            playerEntity.Jump();
+        }
+        else if(jumping && jumpStopTime < Time.time && !Input.GetKey(KeyCode.Space) && !verticalMomentumStopped)
+        {
+            verticalMomentumStopped = true;
+            playerEntity.StopVerticalMomemtum();
+        }
+        else if(jumpStopTime > Time.time && jumping && Input.GetKeyUp(KeyCode.Space))
+        {
+            verticalMomentumStopped = true;
+        }
+	}
+
+    float GetHorizontalInput()
+    {
+        return Input.GetAxisRaw("Horizontal");
+    }
+
+    public void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (coll.collider.CompareTag("Ground"))
+        {
+            verticalMomentumStopped = false;
+            jumping = false;
+        }
+
+        Debug.Log("Not jumping.");
+    }
+}
---
+++ Monotone/Assets/Scenes/Scripts/PlayerEntity.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerEntity : MonoBehaviour {
+
+    private Rigidbody2D rb;
+
+    public float jumpForce;
+    public float movementSpeed;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+
+    public void Move(float hInput)
+    {
+        rb.MovePosition(rb.position + Vector2.right * hInput * movementSpeed * Time.deltaTime);
+    }
+
+    public void Jump()
+    {
+        rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+    }
+
+    public void StopVerticalMomemtum()
+    {
+        Vector2 v = rb.velocity;
+        rb.velocity = new Vector2(v.x, 0f);
+    }
+}
