5700bbcfcb8aea57ffe3fc4c6a66377a42e9fd2b
Esko Evtyukov <eskoonwheels@gmail.com>
2018-10-10 20:27:54 +0300

    Fixed Glow, Added partial text

---
+++ Monotone/Assets/Scripts/LevelGlow.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelGlow : MonoBehaviour {
+
+    private Color defaultColor = Color.white;
+    private Color transparent = new Color(0f, 0f, 0f, 0f);
+
+    private SpriteRenderer spriteRenderer;
+
+    private float lerpProgress;
+
+    public float lerpSpeed = 1f;
+
+    void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    void Update()
+    {
+        lerpProgress += Time.deltaTime * lerpSpeed;
+
+        Color lerpColor = Color.white;
+        lerpColor = Color.Lerp(transparent, defaultColor, Mathf.Sin(Time.time * lerpSpeed));
+        spriteRenderer.color = lerpColor;
+    }
+}
---
+++ Monotone/Assets/textProjector.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+[System.Serializable]
+public class textData
+{
+    public Text text;
+    public Transform target;
+}
+
+public class textProjector : MonoBehaviour {
+
+    public textData[] data;
+
+    void LateUpdate()
+    {
+        foreach (textData d in data)
+            Project(d.text, d.target);
+    }
+
+    public void Project(Text text, Transform target)
+    {
+       text.rectTransform.position = Camera.main.WorldToScreenPoint(target.position);
+    }
+
+}
